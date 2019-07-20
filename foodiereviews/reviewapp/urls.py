6b1eaa443090ff964caf4fbc7425a21b222ec1b7
geraldinetay18 <geraldine.tayhuiling14@gmail.com>
2019-07-20 17:32:15 +0800

    Latest stable release

--- foodiereviews/reviewapp/urls.py
+++ foodiereviews/reviewapp/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
     # ex: /reviewapp/
     path('', views.categories, name='categories'),
 
-    # ex: /reviewapp/2
-    path('<int:category_id>/', views.restaurants, name='restaurants'),
-
     # ex: /reviewapp/search/
     path('search/', views.search, name='search'),
+    
+    # ex: /reviewapp/2
+    path('<int:category_id>/', views.restaurants, name='restaurants'),
 
     # ex: /reviewapp/resto/5
     path('resto/<int:restaurant_id>/', views.details, name='details'),
@@ -26,10 +26,10 @@ urlpatterns = [
     # ex: /reviewapp/reply/6
     path('reply/<int:comment_id>/', views.reply, name='reply'),
 
-    # ex: /reviewapp/resto/5/reviewed
-    path('resto/<int:restaurant_id>/reviewed', views.reviewed, name='reviewed'),
-    
     # ex: /reviewapp/like/6
     path('like/<int:review_id>/', views.like, name='like'),
+
+    # ex: /reviewapp/resto/5/reviewed
+    path('resto/<int:restaurant_id>/reviewed', views.reviewed, name='reviewed'),
 
 ]
--- foodiereviews/reviewapp/views.py
+++ foodiereviews/reviewapp/views.py
@@ -1,9 +1,5 @@
-# no use if have render
-from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import *
 from .forms import *
-from django.template import loader  # no use it for #4
-# replace the 2 imported http above, this is #4
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
@@ -16,6 +12,16 @@ def categories(request):
     return render(request, 'reviewapp/categories.html', context={'categories': categories})
 
 
+def search(request):
+    search_detail = request.GET.get('q')
+    restaurants = Restaurant.objects.filter(restaurant_text__icontains=search_detail)
+    context = {
+        'restaurants': restaurants,
+        'search_detail': search_detail
+    }
+    return render(request, 'reviewapp/restaurants.html', context)
+
+
 def restaurants(request, category_id):
     category = Category.objects.get(id=category_id)
     restaurants = category.get_restaurants()
@@ -26,34 +32,10 @@ def restaurants(request, category_id):
     return render(request, 'reviewapp/restaurants.html', context)
 
 
-def comment(request, review_id):
-    review = get_object_or_404(Review, pk=review_id)
-    if review:
-        review.comment_set.create(comment_user=request.user, comment_description=request.POST.get('comment_description'))
-    else:
-        print("Invalid fields for comment. Required: review id, username and comment description.")
-
-    return render(request, 'reviewapp/details.html', {'restaurant': review.restaurant, 'user': request.user})
-
-
-def reply(request, comment_id):
-    comment = get_object_or_404(Comment, pk=comment_id)
-    if comment:
-        comment.reply_set.create(reply_user=request.user, reply_description=request.POST.get('reply_description'))
-    else:
-        print("Invalid fields for reply. Required: comment id, username and reply description.")
-
-    return render(request, 'reviewapp/details.html', {'restaurant': comment.review.restaurant, 'user': request.user})
-
-
-def like(request, review_id):
-    review = get_object_or_404(Review, pk=review_id)
-    if review:
-        review.review_likes = review.review_likes + 1
-        review.save()
-    else:
-        print("Invalid fields for comment. Required: review id, username and comment description.")
-    return render(request, 'reviewapp/details.html', {'restaurant': review.restaurant, 'user': request.user})
+@login_required
+def details(request, restaurant_id):
+    restaurant = get_object_or_404(Restaurant, pk=restaurant_id)
+    return render(request, 'reviewapp/details.html', {'restaurant': restaurant, 'user': request.user})
 
 
 @login_required
@@ -65,8 +47,42 @@ def add(request, restaurant_id):
         'form': form
     }
     return render(request, 'reviewapp/add.html', context)
+    
+
+@login_required
+def comment(request, review_id):
+    review = get_object_or_404(Review, pk=review_id)
+    if review:
+        review.comment_set.create(comment_user=request.user, comment_description=request.POST.get('comment_description'))
+    else:
+        print("Invalid fields for comment. Required: review id, username and comment description.")
+
+    return render(request, 'reviewapp/details.html', {'restaurant': review.restaurant, 'user': request.user})
 
 
+@login_required
+def reply(request, comment_id):
+    comment = get_object_or_404(Comment, pk=comment_id)
+    if comment:
+        comment.reply_set.create(reply_user=request.user, reply_description=request.POST.get('reply_description'))
+    else:
+        print("Invalid fields for reply. Required: comment id, username and reply description.")
+
+    return render(request, 'reviewapp/details.html', {'restaurant': comment.review.restaurant, 'user': request.user})
+
+
+@login_required
+def like(request, review_id):
+    review = get_object_or_404(Review, pk=review_id)
+    if review:
+        review.review_likes = review.review_likes + 1
+        review.save()
+    else:
+        print("Invalid fields for comment. Required: review id, username and comment description.")
+    return render(request, 'reviewapp/details.html', {'restaurant': review.restaurant, 'user': request.user})
+
+
+@login_required
 def reviewed(request, restaurant_id):
     form = ReviewForm(request.POST)
     if form.is_valid():
@@ -78,13 +94,3 @@ def reviewed(request, restaurant_id):
         print(form.errors)
 
     return render(request, 'reviewapp/details.html', {'restaurant': res, 'user': request.user})
-
-
-@login_required
-def details(request, restaurant_id):
-    restaurant = get_object_or_404(Restaurant, pk=restaurant_id)
-    return render(request, 'reviewapp/details.html', {'restaurant': restaurant, 'user': request.user})
-
-def search(request):
-    search_detail = request.GET.get('q')
-    return HttpResponse("You're looking at the restaurant list that match %s" % search_detail)
